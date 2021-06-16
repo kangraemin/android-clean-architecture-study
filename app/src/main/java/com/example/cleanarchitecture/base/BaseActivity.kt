07dfcd0f3af6bb325bf0958d54fafb8f142e3dad
kangraemin <remin1994@gmail.com>
2021-06-17 00:41:33 +0900

    [Add] Variables for data binding using generic to BaseActivity

--- app/src/main/java/com/example/cleanarchitecture/base/BaseActivity.kt
+++ app/src/main/java/com/example/cleanarchitecture/base/BaseActivity.kt
@@ -1,11 +1,24 @@
 package com.example.cleanarchitecture.base
 
+import android.os.Bundle
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
 import io.reactivex.disposables.CompositeDisposable
 
-abstract class BaseActivity: AppCompatActivity() {
+abstract class BaseActivity<VDB : ViewDataBinding>(
+    @LayoutRes
+    protected val layoutResId: Int
+) : AppCompatActivity() {
 
+    protected lateinit var viewDataBinding: VDB
     protected val compositeDisposable = CompositeDisposable()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewDataBinding = DataBindingUtil.setContentView(this, layoutResId)
+    }
 
     override fun onDestroy() {
         compositeDisposable.dispose()
