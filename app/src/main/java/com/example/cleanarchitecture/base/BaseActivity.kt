ed5aa1601aad21188938b850ea986364b19dec94
kangraemin <remin1994@gmail.com>
2021-06-17 00:22:54 +0900

    [Add] Default BaseActivity
    - Add compositeDisposable for dispose when activity is destroyed

---
+++ app/src/main/java/com/example/cleanarchitecture/base/BaseActivity.kt
@@ -0,0 +1,14 @@
+package com.example.cleanarchitecture.base
+
+import androidx.appcompat.app.AppCompatActivity
+import io.reactivex.disposables.CompositeDisposable
+
+abstract class BaseActivity: AppCompatActivity() {
+
+    protected val compositeDisposable = CompositeDisposable()
+
+    override fun onDestroy() {
+        compositeDisposable.dispose()
+        super.onDestroy()
+    }
+}
