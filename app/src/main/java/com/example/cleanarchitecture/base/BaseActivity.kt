a824befa0ab4fae5b668496395e10d999b19a241
kangraemin <remin1994@gmail.com>
2021-06-17 22:07:21 +0900

    [Add] BaseViewModel variable to base Activity using generic

--- app/src/main/java/com/example/cleanarchitecture/base/BaseActivity.kt
+++ app/src/main/java/com/example/cleanarchitecture/base/BaseActivity.kt
@@ -7,13 +7,14 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import io.reactivex.disposables.CompositeDisposable
 
-abstract class BaseActivity<VDB : ViewDataBinding>(
+abstract class BaseActivity<VDB : ViewDataBinding, VM : BaseViewModel>(
     @LayoutRes
     protected val layoutResId: Int
 ) : AppCompatActivity() {
 
     protected lateinit var viewDataBinding: VDB
     protected val compositeDisposable = CompositeDisposable()
+    protected abstract val viewModel: VM
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
