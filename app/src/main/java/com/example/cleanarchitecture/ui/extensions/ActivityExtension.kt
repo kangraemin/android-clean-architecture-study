33383cb9f776fa28ed88e672021b25af66f0bfe9
kangraemin <remin1994@gmail.com>
2021-06-20 03:59:52 +0900

    [Add] Logics to save cat image when user shake phone
    - Add desugaring options for Instant java class  
    - Add settings to save image in manifest
    - Add extension functions used in save task

--- app/src/main/java/com/example/cleanarchitecture/ui/extensions/ActivityExtension.kt
+++ app/src/main/java/com/example/cleanarchitecture/ui/extensions/ActivityExtension.kt
@@ -1,9 +1,13 @@
 package com.example.cleanarchitecture.ui.extensions
 
+import android.app.Activity
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Build
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 fun AppCompatActivity.hideNavigationUI() {
@@ -20,4 +24,16 @@ fun AppCompatActivity.hideNavigationUI() {
                 View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
                 or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
     }
+}
+
+fun Activity.getBackgroundColor(): Int {
+    return (window.decorView.background as? ColorDrawable)?.color ?: Color.GRAY
+}
+
+fun Activity.showLongToastMessage(message: String) {
+    Toast.makeText(
+        this,
+        message,
+        Toast.LENGTH_LONG
+    ).show()
 }
---
+++ app/src/main/java/com/example/cleanarchitecture/util/ShakeDetector.kt
@@ -0,0 +1,66 @@
+package com.example.cleanarchitecture.util
+
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import kotlin.math.sqrt
+
+
+// http://jasonmcreynolds.com/?p=388
+class ShakeDetector : SensorEventListener {
+    private var listener: OnShakeListener? = null
+    private var shakeTimestamp: Long = 0
+
+    fun setOnShakeListener(listener: OnShakeListener?) {
+        this.listener = listener
+    }
+
+    interface OnShakeListener {
+        fun onShake()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        // ignore
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (listener != null) {
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+            val gX = x / SensorManager.GRAVITY_EARTH
+            val gY = y / SensorManager.GRAVITY_EARTH
+            val gZ = z / SensorManager.GRAVITY_EARTH
+
+            // gForce will be close to 1 when there is no movement.
+            val gForce: Float = sqrt(gX * gX + gY * gY + gZ * gZ)
+            if (gForce > SHAKE_THRESHOLD_GRAVITY) {
+                val now = System.currentTimeMillis()
+                // ignore shake events too close to each other (500ms)
+                if (shakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
+                    return
+                }
+
+                // reset the shake count after 3 seconds of no shakes
+                if (shakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
+                    listener!!.onShake()
+                }
+                shakeTimestamp = now
+            }
+        }
+    }
+
+    companion object {
+        /*
+	    * The gForce that is necessary to register as shake.
+	    * Must be greater than 1G (one earth gravity unit).
+	    * You can install "G-Force", by Blake La Pierre
+	    * from the Google Play Store and run it to see how
+	    *  many G's it takes to register a shake
+	    */
+        private const val SHAKE_THRESHOLD_GRAVITY = 2.7f
+        private const val SHAKE_SLOP_TIME_MS = 500
+        private const val SHAKE_COUNT_RESET_TIME_MS = 3000
+    }
+}
